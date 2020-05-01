206de999e2e3c8bbe01a1d5d295aa984bec1f3ec
gscsdlz <lz842063523@foxmail.com>
2020-05-01 13:28:29 +0800

    chore:add more

--- array_reader.go
+++ array_reader.go
@@ -1,1 +1,15 @@
 package json_reader
+
+type ArrayReader struct{}
+
+func (j *ArrayReader) Get() JsonReader {
+	panic("implement me")
+}
+
+func (j *ArrayReader) Number() float64 {
+	panic("implement me")
+}
+
+func (j *ArrayReader) String() string {
+	panic("implement me")
+}
---
+++ factory.go
@@ -0,0 +1,5 @@
+package json_reader
+
+func Parse(data []byte) JsonReader {
+	return nil
+}
